f9125393b5c82901e7450c146b478514270da1d5
Dante Choppa <110491850+DanteChoppaXXX@users.noreply.github.com>
2024-06-01 06:39:18 -0400

    Update BottomNavBar.jsx

--- src/components/BottomNavBar.jsx
+++ src/components/BottomNavBar.jsx
@@ -1,30 +1,86 @@
 import React from 'react';
 import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
-import { Link } from "react-router-dom";
-//import "../fonts/fonts.css";
+import { useLocation, Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
-//import { Search, FavoriteBorderTwoTone, Event, MonetizationOn, AccountCircle } from '@material-ui/icons';
-import discoverIcon from '../icons/icon_discover.png'; // Replace with your image paths
+import discoverIcon from '../icons/icon_discover.png';
 import forYouIcon from '../icons/icon_favorite.png';
 import myEventIcon from '../icons/icon_ticket.png';
 import sellIcon from '../icons/icon_sell.png';
 import accountIcon from '../icons/icon_account.png';
 
+const useStyles = makeStyles({
+  root: {
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+    zIndex: 100,
+    height: '100px',
+    paddingBottom: '20px'
+  },
+  icon: {
+    width: '30px',
+    height: '30px'
+  },
+  label: {
+    fontSize: '6px',
+    transform: 'scale(0.8)'
+  },
+  active: {
+    color: 'blue !important',
+    '& img': {
+      filter: 'invert(30%) sepia(90%) saturate(1000%) hue-rotate(190deg) brightness(100%) contrast(90%)'
+    }
+  }
+});
 
 const BottomNavBar = () => {
-  const iconStyle = { width: '30px', height: '30px' };
-  const labelStyle = { fontSize: '6px', transform: 'scale(0.8)' }; // Use the desired font size
+  const classes = useStyles();
+  const location = useLocation();
+
+  const getClassName = (path) => {
+    return `${classes.label} ${location.pathname === path ? classes.active : ''}`;
+  };
 
   return (
-    <BottomNavigation showLabels style={{ position: 'fixed', bottom: 0, width: '100%', zIndex: 100, height: '100px', paddingBottom: '20px' }}>
-      <BottomNavigationAction label="Discover" icon={<img src={discoverIcon} alt="Discover" style={iconStyle} />} style={labelStyle} component={Link} to="/myevent" />
-      <BottomNavigationAction label="For You" icon={<img src={forYouIcon} alt="For You" style={iconStyle} />} style={labelStyle} component={Link} to="/email" />
-      <BottomNavigationAction label="My Events" icon={<img src={myEventIcon} alt="My Event" style={iconStyle} />} style={labelStyle} component={Link} to="/myevent" />
-      <BottomNavigationAction label="Sell" icon={<img src={sellIcon} alt="Sell" style={iconStyle} />} style={labelStyle} component={Link} to="/fee" />
-      <BottomNavigationAction label="MyAccount" icon={<img src={accountIcon} alt="My Account" style={iconStyle} />} style={labelStyle} component={Link} to="/myaccount" />
+    <BottomNavigation showLabels className={classes.root}>
+      <BottomNavigationAction
+        label="Discover"
+        icon={<img src={discoverIcon} alt="Discover" className={classes.icon} />}
+        className={getClassName('/home')}
+        component={Link}
+        to="/home"
+      />
+      <BottomNavigationAction
+        label="For You"
+        icon={<img src={forYouIcon} alt="For You" className={classes.icon} />}
+        className={getClassName('/email')}
+        component={Link}
+        to="/email"
+      />
+      <BottomNavigationAction
+        label="My Events"
+        icon={<img src={myEventIcon} alt="My Event" className={classes.icon} />}
+        className={getClassName('/myevent')}
+        component={Link}
+        to="/myevent"
+      />
+      <BottomNavigationAction
+        label="Sell"
+        icon={<img src={sellIcon} alt="Sell" className={classes.icon} />}
+        className={getClassName('/fee')}
+        component={Link}
+        to="/fee"
+      />
+      <BottomNavigationAction
+        label="My Account"
+        icon={<img src={accountIcon} alt="My Account" className={classes.icon} />}
+        className={getClassName('/myaccount')}
+        component={Link}
+        to="/myaccount"
+      />
     </BottomNavigation>
   );
 };
 
 export default BottomNavBar;
-
